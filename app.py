52fc398706d6f464b71cad4d8edb5cc4ff0b9e94
Amrithesh <amritheshkakkoth@gmail.com>
2024-06-28 16:10:32 +0530

    chatbot interface has been established

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import Chroma
+from streamlit_chat import message
 import time
 
 
@@ -32,7 +33,7 @@ if "vector" not in st.session_state:
     st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
 
 
-st.title("CHATBOT TEST")
+st.title("RAG CHATBOT 🤖")
 
 llm=ChatGroq(groq_api_key=groq_api_key,
              model_name="Llama3-8b-8192")
@@ -54,17 +55,41 @@ document_chain =create_stuff_documents_chain(llm,prompt)
 retriever= st.session_state.vectors.as_retriever()
 retrival_chain = create_retrieval_chain(retriever,document_chain)
 
-prompt = st.text_input("Enter your prompt here!")
+prompt = st.text_input("Enter your question here!")
 
+if 'user_input' not in st.session_state:
+    st.session_state['user_input'] = []
+ 
+if 'response' not in st.session_state:
+    st.session_state['response'] = []
 
 if prompt:
     start = time.process_time()
     response = retrival_chain.invoke({"input":prompt})
     print("Response time :",time.process_time()-start)
-    st.write(response['answer'])
+    output=response['answer']
+    st.session_state.response.append(prompt)
+    st.session_state.user_input.append(output)  
 
     with st.expander("Document Similarity Search"):
         for i,doc in enumerate(response["context"]):
             st.write(doc.page_content)
             st.write("-------------------------------------------------")
 
+
+
+message_history = st.empty()
+ 
+if st.session_state['user_input']:
+    for i in range(len(st.session_state['user_input']) - 1, -1, -1):
+        
+        # This function displays user input
+        
+        message(st.session_state["user_input"][i],
+                key=str(i), avatar_style="icons")
+        
+        # This function displays response
+        
+        message(st.session_state['response'][i],
+                avatar_style="miniavs", is_user=True
+                , key=str(i) + 'data_by_user')
